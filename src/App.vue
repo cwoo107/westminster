2c0dea65b80a095eac5f6685e75885604e855b5c
Caleb Woods <woodscaleb@gmail.com>
2019-08-08 06:58:44 -0700

    added more questions

--- src/App.vue
+++ src/App.vue
@@ -401,6 +401,375 @@ comes to pass.
       </b-row>
     </b-container>
 
+    <b-container fluid  id='16' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 16. Did all mankind fall in Adam’s first transgression?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. The covenant being made with Adam
+          <a v-b-popover.click="'Gen. 2:16-17; Jas. 2:10'">
+            [a]
+          </a> 
+           not only for himself, but for his posterity; all mankind, descending from him by ordinary generation, sinned in him, and fell with him, in his first transgression
+          <a v-b-popover.click="'Rom. 5:12-21; 1 Cor. 15:22'">
+            [b]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='17'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 17. Into what estate did the fall bring mankind?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. The fall brought mankind into an estate of sin and misery.
+          <a v-b-popover.click="'Gen. 3:16-19, 23; Rom. 3:16; 5:12; Eph. 2:1'">
+            [a]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='18' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 18. Wherein consists the sinfulness of that estate whereinto man fell?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. The sinfulness of that estate whereinto man fell, consists in the guilt of Adam’s first sin
+          <a v-b-popover.click="'Rom. 5:12, 19'">
+            [a]
+          </a> 
+           the want of original righteousness
+          <a v-b-popover.click="'Rom. 3:10; Col. 3:10; Eph. 4:24'">
+            [b]
+          </a> 
+           and the corruption of his whole nature
+          <a v-b-popover.click="'Ps. 51:5; John 3:6; Rom. 3:18; 8:7-8; Eph. 2:3'">
+            [c]
+          </a> 
+           which is commonly called original sin; together with all actual transgressions which proceed from it
+          <a v-b-popover.click="'Gen. 6:5; Ps. 53:1-3; Matt. 15:19; Rom. 3:10-18, 23; Gal. 5:19-21; Jas. 1:14-15'">
+            [d]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='19'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 19. What is the misery of that estate whereinto man fell?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. All mankind by their fall lost communion with God,
+          <a v-b-popover.click="'Gen. 3:8, 24; John 8:34, 42, 44; Eph. 2:12; 4:18'">
+            [a]
+          </a> 
+           are under his wrath
+          <a v-b-popover.click="' John 3:36; Rom. 1:18; Eph. 2:3; 5:6'">
+            [b]
+          </a> 
+           and curse,
+          <a v-b-popover.click="'Gal. 3:10; Rev. 22:3'">
+            [c]
+          </a> 
+           and so made liable to all miseries in this life,
+          <a v-b-popover.click="'Gen. 3:16-19; Job 5:7; Ecc. 2:22-23; Rom. 8:18-23'">
+            [d]
+          </a>
+          to death
+          <a v-b-popover.click="'Ezek. 18:4; Rom. 5:12; 6:23'">
+            [e]
+          </a>
+          itself, and to the pains of hell for ever
+          <a v-b-popover.click="'Matt. 25:41, 46; 2 Thess. 1:9; Rev. 14:9-11'">
+            [f]
+          </a>
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='20' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 20. Did God leave all mankind to perish in the estate of sin and misery?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. God having, out of his mere good pleasure, from all eternity, elected some to everlasting life
+          <a v-b-popover.click="'Acts 13:48; Eph. 1:4-5; 2 Thess. 2:13-14'">
+            [a]
+          </a> 
+           did enter into a covenant of grace, to deliver them out of the estate of sin and misery, and to bring them into an estate of salvation by a Redeemer
+          <a v-b-popover.click="'Gen. 3:15; 17:7; Ex. 19:5-6; Jer. 31:31-34; Matt. 20:28; 1 Cor. 11:25; Heb. 9:15'">
+            [b]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='21'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 21. Who is the Redeemer of God’s elect?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. The only Redeemer of God’s elect is the Lord Jesus Christ,
+          <a v-b-popover.click="' John 14:6; Acts 4:12; 1 Tim. 2:5-6'">
+            [a]
+          </a> 
+           who, being the eternal Son of God,
+          <a v-b-popover.click="'Ps. 2:7; Matt. 3:17; 17:5; John 1:18'">
+            [b]
+          </a> 
+           became man
+          <a v-b-popover.click="'Isa. 9:6; Matt. 1:23; John 1:14; Gal. 4:4'">
+            [c]
+          </a> 
+           and so was, and continueth to be, God and man in two distinct natures, and one person, forever.
+          <a v-b-popover.click="'Acts 1:11; Heb. 7:24-25'">
+            [d]
+          </a>
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='22' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 22. How did Christ, being the Son of God, become man?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ, the Son of God, became man, by taking to himself a true body, and a reasonable soul,
+          <a v-b-popover.click="'Phil. 2:7; Heb. 2:14, 17'">
+            [a]
+          </a> 
+           being conceived by the power of the Holy Ghost, in the womb of the virgin Mary, and born of her
+          <a v-b-popover.click="'Luke 1:27, 31, 35'">
+            [b]
+          </a> 
+           yet without sin.
+          <a v-b-popover.click="'2 Cor. 5:21; Heb. 4:15; 7:26; 1 John 3:5'">
+            [c]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='23'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 23. What offices doth Christ execute as our Redeemer??</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ, as our Redeemer, executeth the offices of a prophet,
+          <a v-b-popover.click="'Deut. 18:18; Acts 2:33; 3:22-23; Heb. 1:1-2'">
+            [a]
+          </a> 
+           of a priest,
+          <a v-b-popover.click="'Heb. 4:14-15; 5:5-6'">
+            [b]
+          </a> 
+           and of a king,
+          <a v-b-popover.click="'Isa. 9:6; Matt. 1:23; John 1:14; Gal. 4:4'">
+            [c]
+          </a> 
+           both in his estate of humiliation and exaltation.
+          <a v-b-popover.click="'Isa. 9:6-7; Luke 1:32-33; John 18:37; 1 Cor. 15:25'">
+            [d]
+          </a>
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='24' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 24. How doth Christ execute the office of a prophet?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ executeth the office of a prophet, in revealing to us, by his Word
+          <a v-b-popover.click="'Luke 4:18-19, 21; Acts 1:1-2; Heb. 2:3'">
+            [a]
+          </a> 
+           and Spirit
+          <a v-b-popover.click="'John 15:26-27; Acts 1:8; 1 Pet. 1:11'">
+            [b]
+          </a> 
+           the will of God for our salvation.
+          <a v-b-popover.click="'John 4:41-42; 20:30-31'">
+            [c]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='25'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 25. How doth Christ execute the office of a priest?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ executeth the office of a priest, in his once offering up of himself a sacrifice to satisfy divine justice,
+          <a v-b-popover.click="'Isa. 53; Acts 8:32-35; Heb. 9:26-28; 10:12'">
+            [a]
+          </a> 
+           and reconcile us to God;
+          <a v-b-popover.click="'Rom. 5:10-11; 2 Cor. 5:18; Col. 1:21-22'">
+            [b]
+          </a> 
+           and in making continual intercession for us
+          <a v-b-popover.click="'Rom. 8:34; Heb. 7:25; 9:24'">
+            [c]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='26' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 26. How doth Christ execute the office of a king?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ executeth the office of a king, in subduing us to himself, in ruling and defending us,
+          <a v-b-popover.click="'Ps. 110:3; Matt. 28:18-20; John 17:2; Col. 1:13'">
+            [a]
+          </a> 
+           and in restraining and conquering all his and our enemies
+          <a v-b-popover.click="'Ps. 2:6-9; 110:1-2; Matt. 12:28; 1 Cor. 15:24-26; Col. 2:15'">
+            [b]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='27'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 27. Wherein did Christ’s humiliation consist?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ’s humiliation consisted in his being born, and that in a low condition,
+          <a v-b-popover.click="'Luke 2:7; 2 Cor. 8:9; Gal. 4:4'">
+            [a]
+          </a> 
+           made under the law,
+          <a v-b-popover.click="'Gal. 4:4'">
+            [b]
+          </a> 
+           undergoing the miseries of this life,
+          <a v-b-popover.click="' Isa. 53:3; Luke 9:58; John 4:6; 11:35; Heb. 2:18'">
+            [c]
+          </a> 
+           the wrath of God,
+          <a v-b-popover.click="'Ps. 22:1 (Matt. 27:46); Isa. 53:10; 1 John 2:2'">
+            [d]
+          </a>
+           and the cursed death of the cross;
+          <a v-b-popover.click="'Gal. 3:13; Phil. 2:8'">
+            [e]
+          </a>
+           in being buried, and continuing under the power of death for a time.
+          <a v-b-popover.click="'Matt. 12:40; 1 Cor. 15:3-4'">
+            [f]
+          </a>
+          </a>
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='28' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 28. Wherein consisteth Christ’s exaltation?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. Christ’s exaltation consisteth in his rising again from the dead on the third day,
+          <a v-b-popover.click="'1 Cor. 15:4'">
+            [a]
+          </a> 
+           in ascending up into heaven,
+          <a v-b-popover.click="'Ps. 68:18; Acts 1:11; Eph. 4:8'">
+            [b]
+          </a> 
+           in sitting at the right hand
+          <a v-b-popover.click="'Ps. 110:1; Acts 2:33-34; Heb. 1:3'">
+            [c]
+          </a> 
+           of God the Father, and in coming to judge the world at the last day.
+          <a v-b-popover.click="'Matt. 16:27; Acts 17:31'">
+            [d]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='29'  style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 29. How are we made partakers of the redemption purchased by Christ?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. We are made partakers of the redemption purchased by Christ, by the effectual application of it to us by his Holy Spirit.
+          <a v-b-popover.click="' Titus 3:4-7'">
+            [a]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid  id='30' class='blue' style="padding-top:200px; padding-bottom:200px">
+      <b-row >
+        <b-col md='12' lg='4' offset-lg='1' style='padding-bottom:20px'>
+          <h4><b>Q. 30. How doth the Spirit apply to us the redemption purchased by Christ?</b></h4>
+        </b-col>
+        <b-col md='12' lg='4' offset-lg='2'>
+          <h4>
+            A. The Spirit applieth to us the redemption purchased by Christ, by working faith in us,
+          <a v-b-popover.click="'Rom. 10:17; 1 Cor. 2:12-16; Eph. 2:8; Phil. 1:29'">
+            [a]
+          </a> 
+           and thereby uniting us to Christ in our effectual calling.
+          <a v-b-popover.click="'John 15:5; 1 Cor. 1:9; Eph. 3:17'">
+            [b]
+          </a> 
+          </h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
